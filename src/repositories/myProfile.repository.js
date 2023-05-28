a22d1d5065f3c8006b617d7e3f24d9b0e5cf3644
devmatheusdias <dev.matheusdias@gmail.com>
2023-05-28 15:39:44 -0300

    routenew myprofile

--- src/repositories/myProfile.repository.js
+++ src/repositories/myProfile.repository.js
@@ -1,16 +1,24 @@
 import { db } from '../database/database.connection.js';
 
-export async function getUserDB(res){
-    
-    const result = await db.query(`
+export async function getUserDB(res) {
+
+    const userDB = await db.query(`
     SELECT u.*
     FROM users u
     JOIN sessions s ON u.id = s."userId"
     WHERE s.token = $1;
-    `,[res]
+    `, [res]
     )
 
-    return result.rows[0]
-}
+    const userPostsDB = await db.query(`
+    SELECT *
+    FROM posts
+    WHERE userid = (SELECT id FROM users WHERE userid = $1);
+    `, [userDB.rows[0].id])
 
+    const user = {...userDB.rows[0], posts: userPostsDB.rows}
+   console.log(user.posts.length)
+
+    return user
+}
 
